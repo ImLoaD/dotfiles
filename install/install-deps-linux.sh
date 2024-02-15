de1efd5e52188d4ef31388d0ad11efbdf8759b3a
Andrew <ivvoid@gmail.com>
2024-02-15 14:57:26 +0800

    remove nvm & asdf

--- install/install-deps-linux.sh
+++ install/install-deps-linux.sh
@@ -2,5 +2,4 @@ apt-get install the_silver_searcher
 apt-get install tree
 apt-get install youtube-dl
 apt-get install jsonpp
-apt-get install nvm
 apt-get install exa
--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -9,7 +9,6 @@ source_if_exists $DOTFILES/zsh/history.zsh
 source_if_exists $DOTFILES/zsh/git.zsh
 source_if_exists ~/.fzf.zsh
 source_if_exists $DOTFILES/zsh/aliases.zsh
-# source_if_exists $HOME/.asdf/asdf.sh
 source_if_exists /usr/local/etc/profile.d/z.sh
 source_if_exists /opt/homebrew/etc/profile.d/z.sh
 
@@ -41,10 +40,6 @@ export PATH="$PATH:$HOME/Develope/flutter/bin"
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$PATH:/Users/imload/go/bin/
 export PATH="$PATH":"$HOME/.pub-cache/bin"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 eval "$(starship init zsh)"
 
@@ -82,45 +77,6 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-#
-# Run 'nvm use' automatically every time there's 
-# a .nvmrc file in the directory. Also, revert to default 
-# version when entering a directory without .nvmrc
-#
-enter_directory() {
-  if [[ -z $PREV_PWD ]]; then
-    PREV_PWD="/"
-  fi
-
-  if [[ $PWD == $PREV_PWD ]]; then
-    return
-  fi
-
-  if [[ $PWD =~ $PREV_PWD && ! -f ".nvmrc" ]]; then
-    return
-  fi
-
-  PREV_PWD=$PWD
-  if [[ -f ".nvmrc" ]]; then
-    nvm use
-    NVM_DIRTY=true
-  elif [[ $NVM_DIRTY = true ]]; then
-    nvm use default
-    NVM_DIRTY=false
-  fi
-}
-
-enter_on_init() {
-  if [[ ! -f ".nvmrc" ]]; then
-    return
-  fi
-
-  enter_directory
-}
-
-# add hook when directory changed
-add-zsh-hook chpwd enter_directory
 
 # run after terminal init
 enter_on_init
